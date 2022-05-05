0af7ab7391980af6d36c44ca61b093aa9d06ab40
cjoasis <102196653+cjoasis@users.noreply.github.com>
2022-05-04 19:18:49 -0600

    Add files via upload

---
+++ switchasa.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class switchasa {
+
+public static void main(String args[]){
+
+    
+     int day = 2;
+        String dayType;
+        String dayString;
+         
+        switch (day) 
+        {
+            case 1:  dayString = "Lunes";
+                     break;
+            case 2:  dayString = "Martes";
+                     break;
+            case 3:  dayString = "Miercoles";
+                     break;
+            case 4:  dayString = "Jueves";
+                     break;
+            case 5:  dayString = "Viernes";
+                     break;
+            case 6:  dayString = "Sabado";
+                     break;
+            case 7:  dayString = "Domingo";
+                     break;
+            default: dayString = "Dia invalido";
+    
+    }
+       int day = 2;
+        String mesType;
+        String mesString;
+         
+        switch (day) 
+        {
+            case 1:  mesString = "Enero";
+                     break;
+            case 2:  mesString = "Junio";
+                     break;
+            case 3:  mesString = "Agosto";
+                     break;
+            case 4:  mesString = "Noviembre";
+                     break;
+            case 5:  mesString = "Octubre";
+                     break;
+            case 6:  mesString = "Diciembre";
+                     break;
+            case 7:  mesString = "Febrero";
+                     break;
+            
+    
+    }     
+
+
+}    
+}
